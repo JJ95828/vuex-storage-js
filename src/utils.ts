64867cf7e8697503912e455bd97de4a3cf49b00b
wangyunfei828 <1974134619@qq.com>
2021-06-07 18:54:14 +0800

    fix: utils get

--- src/utils.ts
+++ src/utils.ts
@@ -6,8 +6,8 @@ export default class utils implements utilsClass {
     this.storage = storage
   }
   has(key: string) {
-    console.log(this.storage.get(key))
-    return !!this.storage.get(key)
+    console.log(this.get(key))
+    return !!this.get(key)
   }
   get(key: string) {
     return JSON.parse(this.storage.getItem(key))
@@ -15,11 +15,11 @@ export default class utils implements utilsClass {
   set(key: string, value: any) {
     this.storage.setItem(key, JSON.stringify(value))
   }
-  
+
   del(key: string) {
     this.storage.removeItem(key)
   }
-}  
+}
 
 export const localStorage = new utils(window.localStorage)
 export const sessionStorage = new utils(window.sessionStorage)
--- types/index.d.ts
+++ types/index.d.ts
@@ -2,15 +2,15 @@ export { Vue, VueConstructor } from 'vue/types/vue'
 export { Plugin as VuexPlugin, Store } from 'vuex/types/index'
 
 export interface utilsClass {
-    has: (key: string) => boolean
-    get: (key: string) => any
-    set: (key: string, value: any) => void
-    del: (key: string) => void
+  has: (key: string) => boolean
+  get: (key: string) => any
+  set: (key: string, value: any) => void
+  del: (key: string) => void
 }
 
 export interface Options {
-  keys: string | string[]
-  mode: string
+  keys?: string | string[]
+  mode?: string
   stateKey?: string[]
   mutationsKey?: string[]
 }
